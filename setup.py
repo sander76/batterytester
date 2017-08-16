d4140e105c797eea2557f36966e5b41a8eea7b86
sanders76 <s.teunissen@gmail.com>
2017-08-16 13:52:13 +0200

    change setupfile

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from distutils.core import setup
 
 setup(
     name='batterytester',
-    version='1.0',
+    version='1.1',
     packages=['batterytester', 'batterytester.database',
               'batterytester.mylogger', 'batterytester.connector',
-              'batterytester.main_test', 'batterytester.incoming_parser'],
+              'batterytester.main_test', 'batterytester.incoming_parser',
+              'batterytester.test_atom'],
     url='',
     license='',
     author='Sander Teunissen',
