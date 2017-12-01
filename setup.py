c943e94cda8d2305802cb4aa6b86aac30ee7cf23
sanders76 <s.teunissen@gmail.com>
2017-12-01 10:40:37 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from setuptools import find_packages
 setup(
     name='batterytester',
-    version='1.2.6',
+    version='1.2.7',
     packages = find_packages(),
     url='',
     license='',
