872bb7406b9310c40f141f706b80efec594db103
sander <s.teunissen@gmail.com>
2018-01-30 16:42:05 +0100

    change test config

--- batterytester/configs/powerview_ledgate_reference_test.py
+++ batterytester/configs/powerview_ledgate_reference_test.py
@@ -3,7 +3,28 @@ from batterytester.main_test.powerview_ledgate_reference_test import \
     PowerViewLedgateReferenceTest
 
 
-test = PowerViewLedgateReferenceTest(
+class PvLedGateReferenceTest(PowerViewLedgateReferenceTest):
+    def get_sequence(self):
+        _val = (
+            BooleanReferenceAtom(
+                name='open shade',
+                command=self.powerview.open_shade,
+                arguments={'shade_id': 9416},
+                duration=30,
+                reference={'7': False}
+            ),
+            BooleanReferenceAtom(
+                'close shade',
+                command=self.powerview.close_shade,
+                arguments={'shade_id': 9416},
+                duration=30,
+                reference={'7': True}
+            )
+        )
+        return _val
+
+
+test = PvLedGateReferenceTest(
     test_name='ledgate_test',
     loop_count=10,
     serial_port='COM6',
@@ -11,26 +32,28 @@ test = PowerViewLedgateReferenceTest(
     hub_ip='192.168.0.1'
 )
 
+# def get_sequence(*args):
+#     self = args[0]
+#     _val = (
+#         BooleanReferenceAtom(
+#             name='open shade',
+#             command=self.powerview.open_shade,
+#             arguments={'shade_id': 9416},
+#             duration=30,
+#             reference={'7': False}
+#         ),
+#         BooleanReferenceAtom(
+#             'close shade',
+#             command=self.powerview.close_shade,
+#             arguments={'shade_id': 9416},
+#             duration=30,
+#             reference={'7': True}
+#         )
+#     )
+#     return _val
 
-def get_sequence(self: PowerViewLedgateReferenceTest):
-    _val = (
-        BooleanReferenceAtom(
-            name='open shade',
-            command=self.powerview.open_shade,
-            duration=30,
-            reference={'b': False, 'a': True, 'c': True}
-        ),
-        BooleanReferenceAtom(
-            'close shade',
-            command=self.powerview.close_shade,
-            duration=30,
-            reference={'b': True, 'a': False, 'c': True}
-        )
-    )
-    return _val
 
-
-test.get_sequence = get_sequence
+# test.get_sequence = get_sequence
 
 if __name__ == "__main__":
     test.start_test()
