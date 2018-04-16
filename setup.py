1e29357a39966553bc33fe259c8e33094b911c71
sander <s.teunissen@gmail.com>
2018-04-16 15:01:18 +0200

    adding availability option

--- setup.py
+++ setup.py
@@ -2,12 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='batterytester',
-    version='1.3.0',
+    version='1.3.1',
     packages=find_packages(exclude='test'),
     url='',
     license='',
     author='Sander Teunissen',
     author_email='s.teunissen@gmail.com',
     description='',
-    install_requires=['pyserial', 'aiopvapi', 'aiohttp', 'python-slugify']
+    install_requires=['pyserial', 'aiopvapi', 'aiohttp', 'python-slugify',
+                      'slack-sansio[aiohttp']
 )
