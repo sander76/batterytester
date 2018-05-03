a95e21e4d311d84f06dccb8447f59852f272ced3
sander <s.teunissen@gmail.com>
2018-05-03 13:12:52 +0200

    update influx datahandler

--- batterytester/components/datahandlers/influx.py
+++ batterytester/components/datahandlers/influx.py
@@ -8,6 +8,7 @@ from slugify import slugify
 import batterytester.core.helpers.message_subjects as subj
 from batterytester.components.datahandlers.base_data_handler import \
     BaseDataHandler
+from batterytester.core.bus import Bus
 from batterytester.core.helpers.constants import ATTR_TIMESTAMP, KEY_VALUE, \
     ATTR_VALUES, ATTR_SENSOR_NAME
 from batterytester.core.helpers.helpers import FatalTestFailException
@@ -16,14 +17,45 @@ from batterytester.core.helpers.message_data import AtomData
 LOGGER = logging.getLogger(__name__)
 
 
-def line_protocol_fields(measurement: dict):
+class InfluxLineProtocol:
+    """https://docs.influxdata.com/influxdb/v1.5/write_protocols/line_protocol_tutorial/"""
+
+    def __init__(self, measurement, time_stamp, tags: dict = None,
+                 fields: dict = None):
+        """
+
+        :param measurement:
+        :param tags:
+        :param fields:
+        :param time_stamp: float in seconds.
+        """
+        self._measurement = slugify(measurement)
+        self._tags = tags
+        self._fields = fields
+        self._timestamp = time_stamp
+
+    def create_measurement(self):
+        return ' '.join(self.creator())
+
+    def creator(self):
+        if self._tags:
+            yield '{},{}'.format(self._measurement,
+                                 line_protocol_fields(self._tags))
+        else:
+            yield self._measurement
+        if self._fields:
+            yield line_protocol_fields(self._fields)
+        yield str(to_nanoseconds(self._timestamp))
+
+
+def line_protocol_fields(tags: dict):
     """Create the fields and their values.
 
     incoming is a dict with {KEY_VALUE:VALUE} structure.
     """
 
     return ','.join(("{}={}".format(key, value) for key, value in
-                     measurement.items()))
+                     tags.items()))
 
 
 def line_protocol_tags(tags: dict):
@@ -88,64 +120,94 @@ class Influx(BaseDataHandler):
     async def setup(self, test_name, bus):
         self.bus = bus
         self.measurement = test_name
-        # self.bus.add_async_task(
-        #     self._check_and_send_to_database()
-        # )
-        self.bus.add_closing_task(self._flush())
+
+    async def shutdown(self, bus: Bus):
+        await self._flush()
 
     def get_subscriptions(self):
         return (
-            (subj.ATOM_WARMUP, self._store_tag_data),
-            (subj.SENSOR_DATA, self._add_to_database),
+            (subj.ATOM_WARMUP, self._atom_warmup_event),
+            (subj.SENSOR_DATA, self._handle_sensor)
         )
 
-    def _store_tag_data(self, subject, data: AtomData):
-        LOGGER.debug("storing tag data")
+    def add_to_buffer(self, data: InfluxLineProtocol):
+        """Add influx line data to buffer and check size"""
+        self.data.append(data)
+        if len(self.data) > self.buffer_size:
+            self._flush()
+
+    def _atom_warmup_event(self, subject, data: AtomData):
         self._tags['loop'] = data.loop.value  # data[KEY_ATOM_LOOP][KEY_VALUE]
         self._tags['idx'] = data.idx.value  # data[KEY_ATOM_INDEX][KEY_VALUE]
 
-        _fields = line_protocol_fields({'value': {KEY_VALUE: 1}})
-        _tags = {
-            'name': slugify(data.atom_name.value),
-            'loop': data.loop.value,
-            'idx': data.idx.value
-        }
-        _tags = line_protocol_tags(_tags)
-        self.data.append(self._create_measurement(
-            _fields, _tags, to_nanoseconds(data.started.value)))
-        self.check_buffer()
+        annotation_tags = 'loop {},index {}'.format(data.loop.value, data.idx.value)
+        _influx = InfluxLineProtocol(
+            self.measurement,
+            data.started.value,
+            fields={'title': 'atom warmup',
+                    'text': data.atom_name.value,
+                    'tags': annotation_tags}
+        )
+        self.add_to_buffer(_influx)
 
-    def _add_to_database(self, subject, data):
-        # LOGGER.debug("Adding to database buffer")
-        _time_stamp = get_time_stamp(data)
-        _fields = line_protocol_fields(data[ATTR_VALUES][ATTR_VALUES])
-        self._tags[ATTR_SENSOR_NAME] = data[ATTR_SENSOR_NAME]
-        _tags = line_protocol_tags(self._tags)
-        self.data.append(
-            self._create_measurement(_fields, _tags, _time_stamp))
-        self.check_buffer()
+    # def _store_tag_data(self, data: AtomData):
+    #     LOGGER.debug("storing tag data")
+    #     self._tags['loop'] = data.loop.value  # data[KEY_ATOM_LOOP][KEY_VALUE]
+    #     self._tags['idx'] = data.idx.value  # data[KEY_ATOM_INDEX][KEY_VALUE]
+    #
+    #     _fields = line_protocol_fields({'value': {KEY_VALUE: 1}})
+    #     _tags = {
+    #         'name': slugify(data.atom_name.value),
+    #         'loop': data.loop.value,
+    #         'idx': data.idx.value
+    #     }
+    #     _tags = line_protocol_tags(_tags)
+    #     self.data.append(self._create_measurement(
+    #         _fields, _tags, to_nanoseconds(data.started.value)))
+    #     self.check_buffer()
 
-    def _create_measurement(self, fields: str, tags: str, time_stamp):
-        """Transform data according to line format protocol.
+    def _handle_sensor(self, subject, data):
+        influx = InfluxLineProtocol(
+            self.measurement,
+            data[ATTR_TIMESTAMP][ATTR_VALUES],
+            tags=self._tags,
+            fields={data[ATTR_SENSOR_NAME]: data[ATTR_VALUES][ATTR_VALUES]}
+        )
+        self.add_to_buffer(influx)
 
-        <measurement>[,<tag-key>=<tag-value>...] <field-key>=<field-value>[,<field2-key>=<field2-value>...] [unix-nano-timestamp]
+        # # LOGGER.debug("Adding to database buffer")
+        # _time_stamp = get_time_stamp(data)
+        # _fields = line_protocol_fields(data[ATTR_VALUES][ATTR_VALUES])
+        #
+        #
+        #
+        # self._tags[ATTR_SENSOR_NAME] = data[ATTR_SENSOR_NAME]
+        # _tags = line_protocol_tags(self._tags)
+        # self.data.append(
+        #     self._create_measurement(_fields, _tags, _time_stamp))
+        # self.check_buffer()
 
-        test_name,loop=x,index=x temp=82 1465839830100400200
-          |       -------------- -------        |
-          |             |             |         |
-          |             |             |         |
-        +-----------+--------+-+---------+-+---------+
-        |measurement|,tag_set| |field_set| |timestamp|
-        +-----------+--------+-+---------+-+---------+
+    # def _create_measurement(self, fields: str, tags: str, time_stamp):
+    #     """Transform data according to line format protocol.
+    #
+    #     <measurement>[,<tag-key>=<tag-value>...] <field-key>=<field-value>[,<field2-key>=<field2-value>...] [unix-nano-timestamp]
+    #
+    #     test_name,loop=x,index=x temp=82 1465839830100400200
+    #       |       -------------- -------        |
+    #       |             |             |         |
+    #       |             |             |         |
+    #     +-----------+--------+-+---------+-+---------+
+    #     |measurement|,tag_set| |field_set| |timestamp|
+    #     +-----------+--------+-+---------+-+---------+
+    #
+    #     """
+    #     ln = '{}{} {} {}'.format(
+    #         self.measurement, tags, fields, time_stamp)
+    #     return ln
 
-        """
-        ln = '{}{} {} {}'.format(
-            self.measurement, tags, fields, time_stamp)
-        return ln
-
-    def check_buffer(self):
-        if len(self.data) > self.buffer_size:
-            self._flush()
+    # def check_buffer(self):
+    #     if len(self.data) > self.buffer_size:
+    #         self._flush()
 
     # async def _check_and_send_to_database(self):
     #     """Checks the length of the data list and
@@ -165,7 +227,7 @@ class Influx(BaseDataHandler):
 
     def _prepare_data(self):
         if self.data:
-            _data = '\n'.join(self.data)
+            _data = '\n'.join(infl.create_measurement() for infl in self.data)
             _data += '\n'
             return _data
         return None
--- test/test_influx_database.py
+++ test/test_influx_database.py
@@ -1,33 +1,63 @@
+from unittest.mock import MagicMock, Mock
+
 import pytest
 
 from batterytester.components.datahandlers import Influx
-from batterytester.components.datahandlers.influx import line_protocol_fields, \
-    line_protocol_tags, get_time_stamp
+from batterytester.components.datahandlers.influx import get_time_stamp, \
+    InfluxLineProtocol
 from batterytester.components.sensor.incoming_parser import get_measurement
-from batterytester.core.helpers.constants import ATTR_VALUES
+from batterytester.core.helpers.constants import ATTR_VALUES, ATTR_TIMESTAMP, \
+    ATTR_SENSOR_NAME
+from batterytester.core.helpers.message_data import AtomData, Data
 
 
 @pytest.fixture
 def fake_measurement1():
-    meas = get_measurement('test', {'i': 10, 'b': 1.2})
+    meas = get_measurement('test', 10)
+    meas[ATTR_TIMESTAMP][ATTR_VALUES] = 12345678
     return meas
 
 
-def test_line_protocol_fields(fake_measurement1):
-    _out = line_protocol_fields(fake_measurement1[ATTR_VALUES][ATTR_VALUES])
-    assert _out == 'i=10,b=1.2'
+@pytest.fixture
+def fake_influx():
+    influx = Influx(host='127.0.0.1', buffer_size=5)
+    influx.bus = MagicMock()
+    influx.bus.add_async_task = Mock()
+    influx._send = Mock()
+    influx.test_name = 'fake_test'
+    influx.measurement = 'fake_test'
+
+    return influx
 
 
-def test_line_protocol_tags():
-    _in = {'a': 'sensor1'}
-    _out = line_protocol_tags(_in)
-    assert _out == ',a=sensor1'
+@pytest.fixture
+def fake_atom_data():
+    atom = AtomData('atom1', 0, 0, 5)
+    atom.started = Data(value=12345678)
+    return atom
 
 
-def test_empty_time_protocol_tags():
-    _in = {}
-    _out = line_protocol_tags(_in)
-    assert _out == ''
+MEASUREMENT = 'test measurement'
+SLUGGED = 'test-measurement'
+
+
+def test_influx_line_protocol_notags(fake_measurement1):
+    inf = InfluxLineProtocol(
+        MEASUREMENT,
+        fake_measurement1[ATTR_TIMESTAMP][ATTR_VALUES],
+        fields={'value': fake_measurement1[ATTR_VALUES][ATTR_VALUES]})
+    assert inf._measurement == SLUGGED
+    _measurement = inf.create_measurement()
+    assert _measurement == '{} value=10 12345678000000000'.format(SLUGGED)
+
+
+def test_influx_line_protocol1_nofields(fake_measurement1):
+    inf = InfluxLineProtocol(
+        MEASUREMENT,
+        fake_measurement1[ATTR_TIMESTAMP][ATTR_VALUES],
+        tags={'value': fake_measurement1[ATTR_VALUES][ATTR_VALUES]})
+    _measurement = inf.create_measurement()
+    assert _measurement == '{},value=10 12345678000000000'.format(SLUGGED)
 
 
 def test_get_time_stamp():
@@ -36,19 +66,43 @@ def test_get_time_stamp():
     assert isinstance(ts, int)
 
 
-@pytest.fixture
-def fake_influx():
-    def fake_send(_self, data):
-        return data
-
-    influx = Influx(host='127.0.0.1', buffer_size=5)
-    influx._send = fake_send
-    return influx
-
-
-def test_add_data(fake_influx):
-    measurement = get_measurement('sensor1', {'val': 1})
-    fake_influx._add_to_database('not_important', measurement)
+def test_atom_warmup(fake_influx: Influx, fake_atom_data: AtomData):
+    fake_influx._atom_warmup_event('no subj', fake_atom_data)
     assert len(fake_influx.data) == 1
+    _data = fake_influx.data[0]
+    assert _data._measurement == 'fake-test'
+    assert _data._tags is None
+    assert _data._fields['title'] == 'atom warmup'
+    assert _data._fields['text'] == fake_atom_data.atom_name.value
+    assert _data._fields['tags'] == "loop 0,index 0"
 
-# todo: test url composition.
+
+def test_prepare_data(fake_influx: Influx, fake_atom_data):
+    val = fake_influx._prepare_data()
+    assert val is None
+    fake_influx._atom_warmup_event('no subj', fake_atom_data)
+    val = fake_influx._prepare_data()
+    assert val is not None
+
+
+def test_handle_sensor(fake_influx: Influx, fake_measurement1, fake_atom_data):
+    fake_influx._handle_sensor('no subj', fake_measurement1)
+    assert len(fake_influx.data) == 1
+    _data = fake_influx.data[0]
+    assert _data._measurement == 'fake-test'
+    assert _data._tags == {}
+    assert _data._fields == {fake_measurement1[ATTR_SENSOR_NAME]: 10}
+
+    fake_influx._atom_warmup_event('no subj', fake_atom_data)
+    fake_influx._handle_sensor('no subj', fake_measurement1)
+    assert len(fake_influx.data) == 3
+    _data = fake_influx.data[-1]
+    assert _data._tags == {'loop': 0, 'idx': 0}
+
+
+def test_flush(fake_influx: Influx, fake_measurement1):
+    fake_influx._handle_sensor('no subj', fake_measurement1)
+    assert len(fake_influx.data) == 1
+    fake_influx._flush()
+    assert len(fake_influx.data) == 0
+    fake_influx._send.assert_called_once()
