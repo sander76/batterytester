9e9a62f45766527fd30e477f5d1b5bff9430767d
sander <s.teunissen@gmail.com>
2018-07-13 16:53:51 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="batterytester",
-    version="1.3.9",
+    version="1.3.10",
     packages=find_packages(exclude="test"),
     url="",
     license="",
