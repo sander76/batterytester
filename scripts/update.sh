b5febe82ab3b5174d6bc64b21ab9d8437d3a3988
sander <s.teunissen@gmail.com>
2018-09-14 11:55:14 +0200

    update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
+echo "stopping tester server"
 sudo systemctl stop tester-server
 
+echo "updating repo"
 cd ~/batterytester
 git pull
 cd ~
+echo "updating batterytester"
 sudo pip3 install batterytester/ --upgrade
+echo "starting tester-server"
 sudo systemctl start tester-server
+echo "ready"
